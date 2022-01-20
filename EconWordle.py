ec591cf6fdbfd5388443d8e1432b32ca74fd4a5f
Bruno Lima <brunolimanetto@gmail.com>
2022-01-20 03:41:39 -0300

    EconWordle

---
+++ EconWordle.py
@@ -0,0 +1,7 @@
+# You start with a certain number of COINS
+
+# For each guess, you pay a certain number of COINS to expand the (closeness) range
+
+# For each GREEN you get rewarded a few coins back.
+
+# You can guess while you have coins (probably)
