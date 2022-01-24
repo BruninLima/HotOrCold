a8dc6e0adac876ed5be12ece5896ea76abbb0d34
Bruno Lima <brunolimanetto@gmail.com>
2022-01-24 16:54:34 -0300

    refacts

--- EconWordle.py
+++
@@ -1,9 +0,0 @@
-# TODO
-
-# You start with a certain number of COINS
-
-# For each guess, you pay a certain number of COINS to expand the (closeness) range
-
-# For each GREEN you get rewarded a few coins back.
-
-# You can guess while you have coins (probably)
--- app.py
+++ app.py
@@ -1,4 +1,55 @@
-from app.main import app
+from flask import Flask, render_template, request
+from funcs import get_new_word, valid_word, get_letter_score
 
-if __name__ == "__main__":
-    app.run()
+
+app = Flask(__name__)
+
+secret_word = get_new_word(5)
+
+GAME_START = False
+
+Score = []
+ruleset = 'closeness'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    global ruleset
+    global Score
+
+    if request.method == 'POST':
+
+        if len(request.form) > 1:
+
+            guessed_word = ''.join(request.form[key]
+                                   for key in request.form.keys()).lower()
+
+            print(guessed_word, secret_word)
+
+            if valid_word(guessed_word) == True:
+
+                Current_Score = []
+
+                for i, j in zip(guessed_word, secret_word):
+
+                    Current_Score += [[i, get_letter_score(i, j, ruleset)]]
+
+                Score += [Current_Score]
+
+                return render_template("index.html", score=Score, ruleset=ruleset)
+
+            else:
+                print("Word Not Valid")
+
+                return render_template("index.html", score=Score, warning=True, ruleset=ruleset)
+
+        if len(request.form) == 1:
+
+            ruleset = request.form['ruleset']
+
+            print(" ")
+            print(" RULESET CHANGED TO " + ruleset)
+            print(" ")
+
+    return render_template("index.html", ruleset=ruleset)
